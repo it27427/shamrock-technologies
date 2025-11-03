7046f459a8961ab2da539861dbb4ed0999abcb16
it2427 <istiak.tushar.qtec@gmail.com>
2025-11-03 20:37:20 +0600

    menu

---
+++ assets/js/menu.js
@@ -0,0 +1,13 @@
+const openbtn = document.querySelector("#open-menu-btn");
+const menulist = document.querySelector(".menulist");
+const openbtnnew = document.querySelector(".open");
+const closebtn = document.querySelector(".close");
+
+openbtn.addEventListener("click", () => {
+  menulist.classList.toggle("hidden");
+});
+
+openbtn.addEventListener("click", () => {
+  openbtnnew.classList.toggle("hidden");
+  closebtn.classList.toggle("hidden");
+});
