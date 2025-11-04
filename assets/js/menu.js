637b73049a5de558fb1e77bde27fc3b3ef22500b
it2427 <istiak.tushar.qtec@gmail.com>
2025-11-04 08:14:05 +0600

    dropdown animated

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -7,7 +7,10 @@ menulist.classList.add("menu-closed");
 menulist.classList.add("flex");
 
 openbtn.addEventListener("click", () => {
-  menulist.classList.toggle("hidden");
+  menulist.classList.toggle("menu-closed");
+  menulist.classList.toggle("menu-open");
+
+  // বাটন আইকন টগল করার অংশ
   openbtnnew.classList.toggle("hidden");
   closebtn.classList.toggle("hidden");
 });
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./*.html"],
+//   theme: {
+//     extend: {
+//       screens: {
+//         xs: "25rem", //400px
+//         sm: "37.5rem", //600px
+//         md: "48rem", // 768px
+//         lg: "62rem", // 992px
+//         xl: "75rem", //1200px
+//         "2xl": "85.375rem", //1366px
+//         "3xl": "87.5rem", //1400px
+//         "4xl": "96rem", //1536px
+//         "5xl": "100rem", // 1600px
+//       },
+//     },
+//   },
+// };
