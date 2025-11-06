46891437876cf6acc43e4a71fcbf551d2feb1df3
it2427 <istiak.tushar.qtec@gmail.com>
2025-11-06 22:08:26 +0600

    hero animation done

--- assets/js/home-animation.js
+++ assets/js/home-animation.js
@@ -1,17 +1,43 @@
 const firstHeadings = document.querySelectorAll(".first-heading");
 const secondHeadings = document.querySelectorAll(".second-heading");
+const heroTop = document.querySelectorAll(".herotop");
+const heroDesign = document.querySelectorAll(".herodesign");
 
-gsap.from(firstHeadings, {
+const tl = gsap.timeline({
+  defaults: {
+    duration: 1.5,
+    ease: "power3.out",
+  },
+});
+
+tl.from(firstHeadings, {
   xPercent: -30,
   opacity: 0,
-  duration: 1.5,
-  ease: "power3.out",
 });
 
-gsap.from(secondHeadings, {
-  xPercent: 30,
-  opacity: 0,
-  duration: 1.5,
-  ease: "power3.out",
-  delay: 0.2,
-});
+tl.from(
+  secondHeadings,
+  {
+    xPercent: 30,
+    opacity: 0,
+  },
+  "<0.2"
+);
+
+tl.from(
+  heroTop,
+  {
+    yPercent: 30,
+    opacity: 0,
+  },
+  "<0.3"
+);
+
+tl.from(
+  heroDesign,
+  {
+    yPercent: 50,
+    opacity: 0,
+  },
+  "+=0.2"
+);
