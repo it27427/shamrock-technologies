b2f64fc7f53c2b8928b6f1c5b1f4e89165f31fa4
it2427 <istiak.tushar.qtec@gmail.com>
2025-11-08 19:48:48 +0600

    testimonial-slider

---
+++ assets/js/slider.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper(".testimonialSlider", {
+  // autoplay: {
+  //   delay: 5000,
+  // },
+
+  speed: 5000,
+  allowTouchMove: false,
+  // loop: true,
+
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
