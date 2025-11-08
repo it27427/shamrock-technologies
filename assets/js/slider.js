6d6ed5fb59ecadc9b52291c8163a175a1ceb2f6f
it2427 <istiak.tushar.qtec@gmail.com>
2025-11-08 20:21:28 +0600

    testimonial slider script done

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,35 +1,26 @@
 const swiper = new Swiper(".testimonialSlider", {
-  // autoplay: {
-  //   delay: 5000,
-  // },
-
-  speed: 5000,
+  autoplay: {
+    delay: 5000,
+  },
   allowTouchMove: false,
-  freeMode: true,
-  centerInsufficientSlides: true,
-  centeredSlidesBounds: true,
+  centeredSlides: true,
   controller: {
     inverse: true,
   },
-
-  // loop: true,
-
   slidesPerView: 1,
   spaceBetween: 20,
-
   breakpoints: {
     640: {
       slidesPerView: 2,
       spaceBetween: 24,
     },
-    992: {
+    768: {
       slidesPerView: 3,
     },
-    1024: {
+    992: {
       slidesPerView: 4,
     },
   },
-
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
